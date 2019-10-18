f5c82147cab6c15588b312fb259aaabc78a7bdc5
Matthew Dolan <matt.dolan@babylonhealth.com>
2019-10-18 17:26:36 +0100

    Delete unnecessary file (#16)

--- publishing.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-./gradlew clean
-./gradlew :orbit:assemble
-./gradlew :orbit:publishLibPublicationToPublicRepository
-./gradlew :orbit-android:assembleRelease
-./gradlew :orbit-android:publishLibPublicationToPublicRepository
