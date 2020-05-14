bc7bd934f6c069976ae884bb39148f4b167e2e6b
mikolaj.leszczynski <mikolaj.leszczynski@babylonhealth.com>
2020-05-14 14:47:19 +0200

    Orbit 2 saved state

---
+++ orbit-2-savedstate/orbit-2-savedstate_build.gradle.kts
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2020 Babylon Partners Limited
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+apply(from = "$rootDir/gradle/scripts/jacoco-android.gradle.kts")
+
+android {
+    compileSdkVersion(29)
+    defaultConfig {
+        minSdkVersion(21)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    java {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(ProjectDependencies.kotlinCoroutines)
+
+    implementation(project(":orbit-2-core"))
+
+    implementation(ProjectDependencies.androidLifecycleSavedState)
+
+    // Testing
+    testImplementation(project(":orbit-2-test"))
+    GroupedDependencies.testsImplementation.forEach { testImplementation(it) }
+    GroupedDependencies.testsRuntime.forEach { testRuntimeOnly(it) }
+}
