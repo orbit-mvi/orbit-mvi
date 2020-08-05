eb4dfb9e6bfba119d2b0350a3013365399ddc85f
Matthew Dolan <matt.dolan@babylonhealth.com>
2020-08-05 23:21:15 +0100

    Custom idling resource with no dependency on Android MainLooper

--- orbit-2-viewmodel/src/main/java/com/babylon/orbit2/viewmodel/AndroidIdlingResource.kt
+++ orbit-2-viewmodel/src/main/java/com/babylon/orbit2/viewmodel/AndroidIdlingResource.kt
@@ -1,25 +1,59 @@
 package com.babylon.orbit2.viewmodel
 
 import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.idling.net.UriIdlingResource
+import androidx.test.espresso.IdlingResource.ResourceCallback
 import com.babylon.orbit2.idling.IdlingResource
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicReference
 
 class AndroidIdlingResource : IdlingResource {
-    private val otherResource = UriIdlingResource("orbit-mvi", 100)
+
+    private val counter: AtomicInteger = AtomicInteger(0)
+    private val idle = AtomicBoolean(true)
+
+    private val job = AtomicReference<Job>()
+
+    private var resourceCallback: ResourceCallback? = null
+
+    private val espressoIdlingResource = object : androidx.test.espresso.IdlingResource {
+        override fun getName() = "orbit-mvi"
+
+        override fun isIdleNow() = idle.get()
+
+        override fun registerIdleTransitionCallback(resourceCallback: ResourceCallback?) {
+            this@AndroidIdlingResource.resourceCallback = resourceCallback
+        }
+    }
 
     init {
-        IdlingRegistry.getInstance().register(otherResource)
+        IdlingRegistry.getInstance().register(espressoIdlingResource)
     }
 
     override fun increment() {
-        otherResource.beginLoad("uri")
+        val counterVal = counter.getAndIncrement()
+        if (counterVal == 0) {
+            job.get()?.cancel()
+        }
+        idle.set(false)
     }
 
     override fun decrement() {
-        otherResource.endLoad("uri")
+        val counterVal = counter.decrementAndGet()
+        if (counterVal == 0) {
+            job.getAndSet(GlobalScope.launch {
+                delay(100)
+                idle.set(true)
+                resourceCallback?.onTransitionToIdle()
+            })?.cancel()
+        }
     }
 
     override fun close() {
-        IdlingRegistry.getInstance().unregister(otherResource)
+        IdlingRegistry.getInstance().unregister(espressoIdlingResource)
     }
 }
