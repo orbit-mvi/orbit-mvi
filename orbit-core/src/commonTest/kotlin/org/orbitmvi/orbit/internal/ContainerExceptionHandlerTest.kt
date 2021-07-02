15ee1346e6664d16bb6df1fd1642d3d05950d54a
Oleksii Malovanyi <oleksii.malovanyi@gmail.com>
2021-07-02 12:35:59 +0400

    Fix Settings' `exceptionHandler` to be still counted in test() mode (#50)
    Co-authored-by: Mikolaj Leszczynski <leszczynski.mikolaj@gmail.com>

--- orbit-core/src/commonTest/kotlin/org/orbitmvi/orbit/internal/ContainerExceptionHandlerTest.kt
+++ orbit-core/src/commonTest/kotlin/org/orbitmvi/orbit/internal/ContainerExceptionHandlerTest.kt
@@ -30,7 +30,11 @@ import kotlin.random.Random
 import kotlin.test.AfterTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import kotlin.test.assertTrue
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.reduce
 
 @ExperimentalCoroutinesApi
 internal class ContainerExceptionHandlerTest {
@@ -90,5 +94,79 @@ internal class ContainerExceptionHandlerTest {
         assertEquals(true, scope.isActive)
         assertEquals(1, exceptions.size)
         assertTrue { exceptions.first() is IllegalStateException }
+    }
+
+    @Test
+    fun `with exception handler test() doesn't break`() {
+        val initState = Random.nextInt()
+        val exceptions = mutableListOf<Throwable>()
+        val exceptionHandler = CoroutineExceptionHandler { _, throwable -> exceptions += throwable }
+        val containerHost = object : ContainerHost<Int, Nothing> {
+            override val container = scope.container<Int, Nothing>(
+                initialState = initState,
+                settings = Container.Settings(
+                    exceptionHandler = exceptionHandler,
+                    intentDispatcher = Dispatchers.Unconfined
+                )
+            )
+        }.test(initState, isolateFlow = false)
+        val newState = Random.nextInt()
+
+        runBlocking {
+            containerHost.testIntent {
+                intent {
+                    reduce { throw IllegalStateException() }
+                }
+            }
+            containerHost.testIntent {
+                intent {
+                    reduce { newState }
+                }
+            }
+        }
+
+        containerHost.assert(initState) {
+            states({ newState })
+        }
+        assertEquals(true, scope.isActive)
+        assertEquals(1, exceptions.size)
+        assertTrue { exceptions.first() is IllegalStateException }
+    }
+
+    @Test
+    fun `without exception handler test() does break`() {
+        val initState = Random.nextInt()
+        val containerHost = object : ContainerHost<Int, Nothing> {
+            override val container = scope.container<Int, Nothing>(
+                initialState = initState,
+                settings = Container.Settings(
+                    intentDispatcher = Dispatchers.Unconfined
+                )
+            )
+        }.test(initState, isolateFlow = false)
+
+        runBlocking {
+            assertFailsWith<IllegalStateException> {
+                containerHost.testIntent {
+                    intent {
+                        reduce { throw IllegalStateException() }
+                    }
+                }
+            }
+            // Note: another `intent{}` would still work
+            // as `test()` moves all the job to scope of invocation,
+            // and out of a container's scope (so it's ignored)
+            val newState = Random.nextInt()
+            containerHost.testIntent {
+                intent {
+                    reduce { newState }
+                }
+            }
+            containerHost.assert(initState) {
+                states({ newState })
+            }
+        }
+
+        assertEquals(true, scope.isActive)
     }
 }
