27625b743af09e3cc9cd2c625b080f2f1987672f
Mikolaj Leszczynski <leszczynski.mikolaj@gmail.com>
2021-10-14 12:35:30 +0200

    Add compose helper functions (#83)

---
+++ orbit-compose/src/main/kotlin/org/orbitmvi/orbit/compose/observe.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2021 Mikołaj Leszczyński & Appmattus Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import org.orbitmvi.orbit.Container
+import org.orbitmvi.orbit.ContainerHost
+
+/**
+ * Observe [Container.sideEffectFlow] in a Compose [LaunchedEffect].
+ */
+@SuppressLint("ComposableNaming")
+@Composable
+public fun <STATE : Any, SIDE_EFFECT : Any> ContainerHost<STATE, SIDE_EFFECT>.collectSideEffect(
+    sideEffect: (suspend (sideEffect: SIDE_EFFECT) -> Unit)
+) {
+    LaunchedEffect(this) {
+        launch {
+            container.sideEffectFlow.collect { sideEffect(it) }
+        }
+    }
+}
+
+/**
+ * Observe [Container.stateFlow] in a Compose [LaunchedEffect].
+ */
+@SuppressLint("ComposableNaming")
+@Composable
+public fun <STATE : Any, SIDE_EFFECT : Any> ContainerHost<STATE, SIDE_EFFECT>.collectState(
+    sideEffect: (suspend (state: STATE) -> Unit)
+) {
+    LaunchedEffect(this) {
+        launch {
+            container.stateFlow.collect { sideEffect(it) }
+        }
+    }
+}
+
+/**
+ * Observe [Container.stateFlow] as [State].
+ */
+@Composable
+public fun <STATE : Any, SIDE_EFFECT : Any> ContainerHost<STATE, SIDE_EFFECT>.collectAsState(): State<STATE> =
+    container.stateFlow.collectAsState()
