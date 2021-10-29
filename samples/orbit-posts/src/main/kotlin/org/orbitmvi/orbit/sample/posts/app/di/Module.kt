b0b82baeff3677ea3ce7e274deb9613559c3c754
Matthew Dolan <info@mattdolan.com>
2021-10-30 00:17:28 +0100

    Remove unused imports

--- samples/orbit-posts/src/main/kotlin/org/orbitmvi/orbit/sample/posts/app/di/Module.kt
+++ samples/orbit-posts/src/main/kotlin/org/orbitmvi/orbit/sample/posts/app/di/Module.kt
@@ -20,7 +20,6 @@
 
 package org.orbitmvi.orbit.sample.posts.app.di
 
-import androidx.lifecycle.SavedStateHandle
 import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
