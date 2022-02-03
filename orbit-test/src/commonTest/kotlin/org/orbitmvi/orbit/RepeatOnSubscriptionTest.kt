deb715300b401b0e7387e3a43ac9ca4ce549794d
mikolaj.leszczynski <leszczynski.mikolaj@gmail.com>
2022-02-03 15:16:45 +0100

    Fix detekt

--- orbit-test/src/commonTest/kotlin/org/orbitmvi/orbit/RepeatOnSubscriptionTest.kt
+++ orbit-test/src/commonTest/kotlin/org/orbitmvi/orbit/RepeatOnSubscriptionTest.kt
@@ -16,9 +16,6 @@
 
 package org.orbitmvi.orbit
 
-import kotlin.random.Random
-import kotlin.test.AfterTest
-import kotlin.test.Test
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
@@ -27,6 +24,9 @@ import kotlinx.coroutines.withTimeout
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.reduce
 import org.orbitmvi.orbit.syntax.simple.repeatOnSubscription
+import kotlin.random.Random
+import kotlin.test.AfterTest
+import kotlin.test.Test
 
 @ExperimentalCoroutinesApi
 internal class RepeatOnSubscriptionTest {
