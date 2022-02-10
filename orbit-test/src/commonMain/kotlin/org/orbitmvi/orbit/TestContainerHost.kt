5fb6c57f8498946e4f770cffbc0686fa13dac001
mikolaj.leszczynski <leszczynski.mikolaj@gmail.com>
2022-02-10 09:51:54 +0100

    Adjust copyright

--- orbit-test/src/commonMain/kotlin/org/orbitmvi/orbit/TestContainerHost.kt
+++ orbit-test/src/commonMain/kotlin/org/orbitmvi/orbit/TestContainerHost.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Mikołaj Leszczyński & Appmattus Limited
+ * Copyright 2021-2022 Mikołaj Leszczyński & Appmattus Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
