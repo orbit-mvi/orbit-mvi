bdbdc82f4d0abc27b714ef225bf2ed6b17be893b
GuilhE <gui.delgado@hotmail.com>
2022-04-11 16:38:57 +0100

    Solves NoWildcardImports, ImportOrdering and WildcardImport

--- orbit-compose/src/main/kotlin/org/orbitmvi/orbit/compose/StateFlowExtensions.kt
+++ orbit-compose/src/main/kotlin/org/orbitmvi/orbit/compose/StateFlowExtensions.kt
@@ -17,7 +17,11 @@
 package org.orbitmvi.orbit.compose
 
 import android.annotation.SuppressLint
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
