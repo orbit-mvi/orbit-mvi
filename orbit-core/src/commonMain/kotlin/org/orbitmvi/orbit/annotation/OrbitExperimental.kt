194ded87c68f30f9f410ed38289c650761b825ac
Matthew Dolan <info@mattdolan.com>
2022-12-02 14:35:16 +0000

    Update orbit-core/src/commonMain/kotlin/org/orbitmvi/orbit/annotation/OrbitExperimental.kt

--- orbit-core/src/commonMain/kotlin/org/orbitmvi/orbit/annotation/OrbitExperimental.kt
+++ orbit-core/src/commonMain/kotlin/org/orbitmvi/orbit/annotation/OrbitExperimental.kt
@@ -12,9 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * File modified by Mikołaj Leszczyński & Appmattus Limited
- * See: https://github.com/orbit-mvi/orbit-mvi/compare/c5b8b3f2b83b5972ba2ad98f73f75086a89653d3...main
  */
 package org.orbitmvi.orbit.annotation
 
