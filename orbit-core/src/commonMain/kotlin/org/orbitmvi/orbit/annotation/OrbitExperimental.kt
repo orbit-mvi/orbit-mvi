988762cd9276acb1c90da8d07a5e9f7b592bfee7
Mikolaj Leszczynski <leszczynski.mikolaj@gmail.com>
2022-12-02 14:56:35 +0100

    Update orbit-core/src/commonMain/kotlin/org/orbitmvi/orbit/annotation/OrbitExperimental.kt
    Co-authored-by: Andy Byrnes <abyrnesdev@gmail.com>

--- orbit-core/src/commonMain/kotlin/org/orbitmvi/orbit/annotation/OrbitExperimental.kt
+++ orbit-core/src/commonMain/kotlin/org/orbitmvi/orbit/annotation/OrbitExperimental.kt
@@ -18,7 +18,7 @@
  */
 package org.orbitmvi.orbit.annotation
 
-@RequiresOptIn(message = "This in an experimental API and may be subject to change.", level = RequiresOptIn.Level.WARNING)
+@RequiresOptIn(message = "This is an experimental API and may be subject to change.", level = RequiresOptIn.Level.WARNING)
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 public annotation class OrbitExperimental
