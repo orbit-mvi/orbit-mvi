cbd84b1561fec055c0c4a2991d149b79901017bd
Matthew Dolan <info@mattdolan.com>
2023-05-17 13:19:07 +0100

    Update to Java 11

--- samples/orbit-posts/orbit-posts_build.gradle.kts
+++ samples/orbit-posts/orbit-posts_build.gradle.kts
@@ -29,8 +29,8 @@ android {
     namespace = "org.orbitmvi.orbit.sample.posts"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     compileSdk = 33
