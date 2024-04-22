261212f85907199b109988137ef0ac1c16d9ec01
Mikolaj Leszczynski <leszczynski.mikolaj@gmail.com>
2024-04-22 18:52:23 +0200

    Improve sealed class state support (#214)
    I got @mattmook 's approval offline and he's travelling, so merging this in :)

---
+++ orbit-core/src/commonTest/kotlin/org/orbitmvi/orbit/internal/RunOnTest.kt
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2024 Mikołaj Leszczyński & Appmattus Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * File modified by Mikołaj Leszczyński & Appmattus Limited
+ * See: https://github.com/orbit-mvi/orbit-mvi/compare/c5b8b3f2b83b5972ba2ad98f73f75086a89653d3...main
+ */
+
+package org.orbitmvi.orbit.internal
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.test.runTest
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.annotation.OrbitExperimental
+import org.orbitmvi.orbit.container
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.reduce
+import org.orbitmvi.orbit.syntax.simple.runOn
+import org.orbitmvi.orbit.test.test
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+internal class RunOnTest {
+
+    @Test
+    fun `runOn does not run when state does not match expected`() = runTest {
+        val middleware = Middleware(backgroundScope)
+
+        middleware.test(this) {
+            expectState { TestState.Loading }
+
+            val job = middleware.doIfInReadyState()
+
+            job.join()
+
+            expectNoItems()
+        }
+    }
+
+    @Test
+    fun `runOn runs when state matches expected`() = runTest {
+        val middleware = Middleware(backgroundScope)
+
+        middleware.test(this) {
+            expectState { TestState.Loading }
+
+            middleware.changeToState(TestState.Ready(42))
+            expectState { TestState.Ready(42) }
+
+            middleware.doIfInReadyState()
+            expectState { TestState.Ready(43) }
+        }
+    }
+
+    @Test
+    fun `runOn does not run when state matches expected but predicate does not match`() = runTest {
+        val middleware = Middleware(backgroundScope)
+
+        middleware.test(this) {
+            expectState { TestState.Loading }
+
+            middleware.changeToState(TestState.Ready(42))
+            expectState { TestState.Ready(42) }
+
+            val job = middleware.doIfInReadyState(predicate = { it.id % 2 == 1 })
+
+            job.join()
+            expectNoItems()
+        }
+    }
+
+    @Test
+    fun `runOn runs when state matches expected and predicate matches`() = runTest {
+        val middleware = Middleware(backgroundScope)
+
+        middleware.test(this) {
+            expectState { TestState.Loading }
+
+            middleware.changeToState(TestState.Ready(42))
+            expectState { TestState.Ready(42) }
+
+            val job = middleware.doIfInReadyState(predicate = { it.id % 2 == 0 })
+            expectState { TestState.Ready(43) }
+
+            job.join()
+            expectNoItems()
+        }
+    }
+
+    @Test
+    fun `runOn cancels when state stops matching expected`() = runTest {
+        val middleware = Middleware(backgroundScope)
+
+        middleware.test(this) {
+            expectState { TestState.Loading }
+
+            middleware.changeToState(TestState.Ready(42))
+            expectState { TestState.Ready(42) }
+
+            val job = middleware.collectIfInReadyState()
+
+            middleware.channel.send(123) // Sending something to ensure intent is started
+            assertEquals(123, middleware.collectorChannel.receive())
+
+            middleware.changeToState(TestState.Loading)
+            expectState { TestState.Loading }
+
+            job.join() // This will never finish if the `collect` is still running
+        }
+    }
+
+    @Test
+    fun `runOn cancels when predicate stops matching`() = runTest {
+        val middleware = Middleware(backgroundScope)
+
+        middleware.test(this) {
+            expectState { TestState.Loading }
+
+            middleware.changeToState(TestState.Ready(42))
+            expectState { TestState.Ready(42) }
+
+            val job = middleware.collectIfInReadyState(predicate = { it.id % 2 == 0 })
+
+            middleware.channel.send(123) // Sending something to ensure intent is started
+            assertEquals(123, middleware.collectorChannel.receive())
+
+            middleware.changeToState(TestState.Ready(43))
+            expectState { TestState.Ready(43) }
+
+            job.join() // This will never finish if the `collect` is still running
+        }
+    }
+
+    @Test
+    fun `runOn does not cancel when predicate keeps matching on subsequent emissions`() = runTest {
+        val middleware = Middleware(backgroundScope)
+
+        middleware.test(this) {
+            expectState { TestState.Loading }
+
+            middleware.changeToState(TestState.Ready(42))
+            expectState { TestState.Ready(42) }
+
+            val job = middleware.collectIfInReadyState(predicate = { it.id % 2 == 0 })
+
+            middleware.channel.send(123) // Sending something to ensure intent is started
+            assertEquals(123, middleware.collectorChannel.receive())
+
+            middleware.changeToState(TestState.Ready(50))
+            expectState { TestState.Ready(50) }
+
+            assertTrue(job.isActive)
+            job.cancel()
+        }
+    }
+
+    sealed interface TestState {
+        object Loading : TestState
+        data class Ready(val id: Int = 42) : TestState
+    }
+
+    @OptIn(OrbitExperimental::class)
+    private inner class Middleware(scope: CoroutineScope) : ContainerHost<TestState, String> {
+        override val container = scope.container<TestState, String>(TestState.Loading)
+
+        val channel: Channel<Int> = Channel()
+        val collectorChannel: Channel<Int> = Channel()
+
+        fun changeToState(state: TestState) = intent {
+            reduce {
+                state
+            }
+        }
+
+        fun doIfInReadyState(predicate: (TestState.Ready) -> Boolean = { true }) = intent {
+            runOn(TestState.Ready::class, predicate = predicate) {
+                reduce {
+                    state.copy(id = state.id + 1)
+                }
+            }
+        }
+
+        fun collectIfInReadyState(predicate: (TestState.Ready) -> Boolean = { true }) = intent {
+            runOn(TestState.Ready::class, predicate = predicate) {
+                channel.consumeAsFlow()
+                    .collect(collectorChannel::send)
+            }
+        }
+    }
+}
--- website/docs/Core/overview.md
+++ website/docs/Core/overview.md
@@ -111,13 +111,15 @@ class ExampleViewModel(
 The Core module contains built-in Orbit operators. Here's how
 they map to MVI concepts:
 
-| MVI Operation      | Orbit DSL                      | Purpose                                                                       |
-|--------------------|--------------------------------|-------------------------------------------------------------------------------|
-| block              | `intent { ... }`               | Contains business logic journeys, allows you to invoke other operators within |
-| transformation     | operations within `intent`     | Run business operations to transform data                                     |
-| posted side effect | `postSideEffect(...)`          | Sends one-off events to the side effect channel                               |
-| reduction          | `reduce { ... }`               | Atomically updates the Container's  state                                     |
-| -                  | `repeatOnSubscription { ... }` | Helps collect infinite flows only when there are active subscribers           |
+| MVI Operation      | Orbit DSL                      | Purpose                                                                                                   |
+|--------------------|--------------------------------|-----------------------------------------------------------------------------------------------------------|
+| block              | `intent { ... }`               | Contains business logic, allows you to invoke other operators within                                      |
+| transformation     | operations within `intent`     | Run business operations to transform data                                                                 |
+| posted side effect | `postSideEffect(...)`          | Sends one-off events to the side effect channel                                                           |
+| reduction          | `reduce { ... }`               | Atomically updates the Container's  state                                                                 |
+| -                  | `repeatOnSubscription { ... }` | Helps collect infinite flows only when there are active subscribers                                       |
+| -                  | `subIntent { ... }`            | Use this to break big `intent` blocks into smaller parts, or for parallel decomposition.                  |
+| -                  | `runOn { ... }`                | Useful for working with sealed class states. The block is only executed if (and while) the state matches. |
 
 Operators are invoked through the `intent` block in a
 [ContainerHost](pathname:///dokka/orbit-core/org.orbitmvi.orbit/-container-host/).
@@ -237,6 +239,100 @@ observes the state or sideEffect streams.
 `repeatOnSubscription` provides functionality to start (and restart) its inner
 block when the state or sideEffect streams are being observed and stop when that
 is no longer the case.
+
+### Sub-intent
+
+``` kotlin
+class Example : ContainerHost<ExampleState, ExampleSideEffect> {
+    ...
+    
+    override val container = scope.container<ExampleState, ExampleSideEffect>(
+        ExampleState()
+    ) {
+        // Remember that this `onCreate` block is an implicit `intent`.
+        // A common pattern is to subscribe to various subintents collecting
+        // flows or other work here.
+        coroutineScope {
+            launch { smallIntent() }
+            launch { smallIntent2() }
+            launch { collectFlow() }
+        }
+    }
+
+    fun largeIntent() = intent {
+        smallIntent()
+        smallIntent2()
+    }
+
+    fun largeIntentWithParallelDecomposition() = intent {
+        coroutineScope {
+            launch { smallIntent() }
+            launch { smallIntent2() }
+        }
+    }
+    
+    suspend fun smallIntent() = subIntent {
+        // ... do work
+        reduce { state.copy(...) }
+    }
+
+    suspend fun smallIntent2() = subIntent {
+        // ... do work
+        reduce { state.copy(...) }
+    }
+    
+    suspend fun collectFlow() = subIntent {
+        someFlow.collect {
+            // ... do work
+            reduce { state.copy(...) }
+        }
+    }
+}
+```
+
+Think of `subIntent` as a suspend function, but with access to Orbit syntax.
+
+It is useful for breaking down large `intent` blocks into smaller parts, or for
+parallel decomposition of work.
+
+Most commonly, you would combine several `subIntent` calls in a single 
+`onCreate` block of the container factory. This is a common pattern when 
+collecting several flows or performing several parallel operations on creation.
+
+### RunOn
+
+``` kotlin
+class Example : ContainerHost<ExampleSealedClassState, ExampleSideEffect> {
+    ...
+    
+    override val container = 
+        scope.container<ExampleSealedClassState, ExampleSideEffect>(
+            ExampleSealedClassState.Loading
+        )
+
+    fun withoutRunOn() = intent {
+        state as? ExampleSealedClassState.Ready?.let {
+            ... run some operations with the captured state
+        }
+    }
+
+     fun withRunOn() = intent {
+        runOn(ExampleSealedClassState.Ready::class) {
+            ... run some operations with the captured state
+        }
+    }
+}
+```
+
+`runOn` is a helper DSL to improve working with sealed class states. It allows
+you to run a block of code only when the state matches the provided type and
+optionally the predicate.
+
+Additionally, the block is automatically cancelled when the state no longer 
+matches the provided type or predicate.
+
+Note that there are no atomicity guarantees when using `runOn`. The block may
+get executed partially.
 
 ### Operator context
 
