74847d4d07405d62fa2c46e97be5402a0e5201a2
Matthew Dolan <info@mattdolan.com>
2025-03-27 13:09:08 +0000

    Remove OrbitTestContext interface to enable expectStateOn function for better testing syntax

--- orbit-test/src/commonMain/kotlin/org/orbitmvi/orbit/test/OrbitTestContext.kt
+++ orbit-test/src/commonMain/kotlin/org/orbitmvi/orbit/test/OrbitTestContext.kt
@@ -16,17 +16,35 @@
 
 package org.orbitmvi.orbit.test
 
+import app.cash.turbine.ReceiveTurbine
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.runBlocking
 import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.annotation.OrbitInternal
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+import kotlin.test.fail
 
-public interface OrbitTestContext<STATE : Any, SIDE_EFFECT : Any, CONTAINER_HOST : ContainerHost<STATE, SIDE_EFFECT>> {
-
-    public val containerHost: CONTAINER_HOST
+public class OrbitTestContext<STATE : Any, SIDE_EFFECT : Any, CONTAINER_HOST : ContainerHost<STATE, SIDE_EFFECT>>(
+    public val containerHost: CONTAINER_HOST,
+    private val resolvedInitialState: STATE,
+    private val emissions: ReceiveTurbine<Item<STATE, SIDE_EFFECT>>,
+    private val settings: TestSettings
+) {
+    @PublishedApi
+    internal var currentConsumedState: STATE = resolvedInitialState
 
     /**
      * Invoke `onCreate` lambda for the [ContainerHost].
      */
-    public fun runOnCreate(): Job
+    public fun runOnCreate(): Job {
+        @OptIn(OrbitInternal::class)
+        val onCreate = containerHost.container.findOnCreate()
+        return runBlocking {
+            @OptIn(OrbitInternal::class)
+            containerHost.container.orbit(onCreate)
+        }
+    }
 
     /**
      * Sanity check assertion. Checks if the initial state is emitted and matches
@@ -37,9 +55,18 @@ public interface OrbitTestContext<STATE : Any, SIDE_EFFECT : Any, CONTAINER_HOST
      */
     @Deprecated(
         "Initial state is now checked automatically. If you wish to manually verify the initial state, use " +
-            "`autoCheckInitialState=false` in test settings and use `expectState` or `awaitState`"
+                "`autoCheckInitialState=false` in test settings and use `expectState` or `awaitState`"
     )
-    public suspend fun expectInitialState()
+    public suspend fun expectInitialState() {
+        if (settings.autoCheckInitialState) {
+            println(
+                "Initial state is now checked automatically. If you wish to manually verify the initial state, use " +
+                        "`autoCheckInitialState=false` in test settings and use `expectState` or `awaitState`"
+            )
+        } else {
+            assertEquals(resolvedInitialState, awaitState())
+        }
+    }
 
     /**
      * Awaits for a state and checks if it matches the expected state change from the
@@ -49,7 +76,9 @@ public interface OrbitTestContext<STATE : Any, SIDE_EFFECT : Any, CONTAINER_HOST
      *
      * @throws AssertionError if state does not match the expected change.
      */
-    public suspend fun expectState(expectedChange: STATE.() -> STATE)
+    public suspend fun expectState(expectedChange: STATE.() -> STATE) {
+        assertEquals(expectedChange(currentConsumedState), awaitState())
+    }
 
     /**
      * Awaits for a state and checks if it matches the expected state.
@@ -58,7 +87,23 @@ public interface OrbitTestContext<STATE : Any, SIDE_EFFECT : Any, CONTAINER_HOST
      *
      * @throws AssertionError if state does not match the expected state.
      */
-    public suspend fun expectState(expected: STATE)
+    public suspend fun expectState(expected: STATE) {
+        assertEquals(expected, awaitState())
+    }
+
+    /**
+     * Awaits for a state and checks if it matches the expected state change from the
+     * previous state, ensuring the last state is of the expected type.
+     *
+     * @param expectedChange The expected change from the previous state.
+     *
+     * @throws AssertionError if state does not match the expected change.
+     */
+    public suspend inline fun <reified LAST_STATE : STATE> expectStateOn(expectedChange: LAST_STATE.() -> STATE) {
+        val currentConsumedState = currentConsumedState
+        assertIs<LAST_STATE>(currentConsumedState)
+        assertEquals(expectedChange(currentConsumedState), awaitState())
+    }
 
     /**
      * Awaits for a side effect and checks if it matches the expected side effect
@@ -67,20 +112,26 @@ public interface OrbitTestContext<STATE : Any, SIDE_EFFECT : Any, CONTAINER_HOST
      *
      * @throws AssertionError if state does not match the expected change.
      */
-    public suspend fun expectSideEffect(expected: SIDE_EFFECT)
+    public suspend fun expectSideEffect(expected: SIDE_EFFECT) {
+        assertEquals(expected, awaitSideEffect())
+    }
 
     /**
      * Assert there are no unconsumed items
      *
      * @throws AssertionError if unconsumed items are found
      */
-    public suspend fun expectNoItems()
+    public suspend fun expectNoItems() {
+        emissions.expectNoEvents()
+    }
 
     /**
      * Return the next item received.
      * This function will suspend if no items have been received.
      */
-    public suspend fun awaitItem(): Item<STATE, SIDE_EFFECT>
+    public suspend fun awaitItem(): Item<STATE, SIDE_EFFECT> {
+        return emissions.awaitItem()
+    }
 
     /**
      * Return the next state received.
@@ -88,7 +139,11 @@ public interface OrbitTestContext<STATE : Any, SIDE_EFFECT : Any, CONTAINER_HOST
      *
      * @throws AssertionError if the most recent item was not a state.
      */
-    public suspend fun awaitState(): STATE
+    public suspend fun awaitState(): STATE {
+        val item = awaitItem()
+        return (item as? Item.StateItem)?.value?.also { currentConsumedState = it }
+            ?: fail("Expected State but got $item")
+    }
 
     /**
      * Return the next side effect received.
@@ -96,17 +151,26 @@ public interface OrbitTestContext<STATE : Any, SIDE_EFFECT : Any, CONTAINER_HOST
      *
      * @throws AssertionError if the most recent item was not a side effect.
      */
-    public suspend fun awaitSideEffect(): SIDE_EFFECT
+    public suspend fun awaitSideEffect(): SIDE_EFFECT {
+        val item = awaitItem()
+
+        return (item as? Item.SideEffectItem)?.value ?: fail("Expected Side Effect but got $item")
+    }
 
     /**
      * Assert that [count] items were received and ignore them.
      * This function will suspend if no items have been received.
      */
-    public suspend fun skipItems(count: Int)
+    public suspend fun skipItems(count: Int) {
+        emissions.skipItems(count)
+    }
 
     /**
      * Finish this test and ignore any events which have already been received.
      * This also cancels any in-progress intents.
      */
-    public suspend fun cancelAndIgnoreRemainingItems()
+    public suspend fun cancelAndIgnoreRemainingItems() {
+        emissions.cancelAndIgnoreRemainingEvents()
+        containerHost.container.cancel()
+    }
 }
