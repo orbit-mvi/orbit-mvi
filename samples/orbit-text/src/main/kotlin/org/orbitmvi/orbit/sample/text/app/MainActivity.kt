213e99eb6c2e348db4d80a111877e463e4a49298
Matthew Dolan <info@mattdolan.com>
2025-05-22 22:26:12 +0200

    Example of Compose TextField state hoisting (#283)
    * Example of textfield state hoisting
    
    * Remove unused dependency
    
    * Update website/docs/Compose/index.md
    
    ---------
    
    Co-authored-by: Mikolaj Leszczynski <leszczynski.mikolaj@gmail.com>

--- samples/orbit-text/src/main/kotlin/org/orbitmvi/orbit/sample/text/app/MainActivity.kt
+++ samples/orbit-text/src/main/kotlin/org/orbitmvi/orbit/sample/text/app/MainActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2023 Mikołaj Leszczyński & Appmattus Limited
+ * Copyright 2022-2025 Mikołaj Leszczyński & Appmattus Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,41 +17,81 @@
 package org.orbitmvi.orbit.sample.text.app
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import org.orbitmvi.orbit.compose.collectAsState
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
     private val viewModel = TextViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(
-            ComposeView(this).apply {
-                setContent {
-                    val state by viewModel.collectAsState()
+        setContent {
+            val state by viewModel.collectAsState()
+            val scaffoldState = rememberScaffoldState()
 
-                    Column {
-                        TextField(
-                            label = { Text("Bad") },
-                            value = state.badField,
-                            onValueChange = { viewModel.updateTextBad(it) }
-                        )
+            Scaffold(
+                scaffoldState = scaffoldState
+            ) { padding ->
+                Column(
+                    modifier = Modifier
+                        .padding(padding)
+                        .windowInsetsPadding(WindowInsets.safeDrawing)
+                        .padding(16.dp)
+                ) {
+                    TextField(
+                        label = { Text("Bad") },
+                        value = state.badField,
+                        onValueChange = { viewModel.updateTextBad(it) }
+                    )
 
-                        TextField(
-                            label = { Text("Good") },
-                            value = state.goodField,
-                            onValueChange = { viewModel.updateTextGood(it) }
-                        )
-                    }
+                    TextField(
+                        label = { Text("Good") },
+                        value = state.goodField,
+                        onValueChange = { viewModel.updateTextGood(it) },
+                        modifier = Modifier.padding(top = 16.dp)
+                    )
+
+                    Box(
+                        modifier = Modifier
+                            .padding(vertical = 16.dp)
+                            .fillMaxWidth()
+                            .height(1.dp)
+                            .background(Color.Gray)
+                    )
+
+                    Text("Demonstration of TextField state hoisting; validation occurs in the view model.")
+                    TextField(
+                        label = { Text(if (state.isTextFieldStateInError) "TextFieldState*" else "TextFieldState") },
+                        state = state.textFieldState,
+                        isError = state.isTextFieldStateInError,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+
+                    Button(onClick = { viewModel.submit() }) { Text("Submit") }
+                    Text(state.result)
                 }
             }
-        )
+        }
     }
 }
