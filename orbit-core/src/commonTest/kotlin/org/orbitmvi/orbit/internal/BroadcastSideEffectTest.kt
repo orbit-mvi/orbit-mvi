6636c57b67cbabdb131581b15af46c99aff13b1a
Matthew Dolan <info@mattdolan.com>
2026-05-13 23:03:16 +0100

    Update copyright messaging
    Co-authored-by: Matthew Dolan <info@mattdolan.com>

--- orbit-core/src/commonTest/kotlin/org/orbitmvi/orbit/internal/BroadcastSideEffectTest.kt
+++ orbit-core/src/commonTest/kotlin/org/orbitmvi/orbit/internal/BroadcastSideEffectTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Mikołaj Leszczyński & Appmattus Limited
+ * Copyright 2026 Mikołaj Leszczyński & Appmattus Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
